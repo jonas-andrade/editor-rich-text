cf718bf033dbe3231404891b988f395abe6c7667
Jonas-andrade <jonas6116m@outlook.com>
2021-07-01 12:19:31 -0300

    add functions events in buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+var textarea = document.querySelector('.area');
+
+var upcase = document.querySelector('#up-arrow');
+var downcase = document.querySelector('#down-arrow');
+var negrito = document.querySelector('#negrito');
+var italico = document.querySelector('#italico');
+var sublinhado = document.querySelector('#sublinhado');
+
+
+
+
+
+
+
+
+
+upcase.addEventListener('click',()=>{
+	var textSelected = textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
+	textarea.value = textarea.value.replace(textSelected,textSelected.toUpperCase());
+})
+
+downcase.addEventListener('click',()=>{
+	var textSelected = textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
+	textarea.value = textarea.value.replace(textSelected,textSelected.toLowerCase());
+})
+negrito.addEventListener('click',()=>{
+	var textSelected = textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
 	
+	textarea.value = textarea.value.replace(textSelected,textSelected.bold());
+	
+})
