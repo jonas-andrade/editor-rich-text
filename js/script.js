84229d0b46d110225759277e5f34806286ef1a74
Jonas-andrade <jonas6116m@outlook.com>
2021-07-02 20:03:53 -0300

    add toggle color buttons up and down

--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
 var textarea = document.querySelector('.area');
 
+
 var upcase = document.querySelector('#up-arrow');
 var downcase = document.querySelector('#down-arrow');
 var negrito = document.querySelector('#negrito');
 var italico = document.querySelector('#italico');
 var sublinhado = document.querySelector('#sublinhado');
 
-
-
-
-
-
-
+var textSelected = window.getSelection();
 
 
 upcase.addEventListener('click',()=>{
-	var textSelected = textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
-	textarea.value = textarea.value.replace(textSelected,textSelected.toUpperCase());
+		downcase.classList.remove("toggleColor")
+		upcase.classList.toggle('toggleColor')
+ 	 	textarea.innerHTML = textarea.innerText.toString().replace(textSelected,textSelected.toString().toUpperCase())
+	
+	
+		
+
 })
 
 downcase.addEventListener('click',()=>{
-	var textSelected = textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
-	textarea.value = textarea.value.replace(textSelected,textSelected.toLowerCase());
+	  upcase.classList.remove("toggleColor")
+		downcase.classList.toggle('toggleColor')
+	 	textarea.innerHTML = textarea.innerText.toString().replace(textSelected,textSelected.toString().toLowerCase())
+
+
 })
+
 negrito.addEventListener('click',()=>{
-	var textSelected = textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
-	
-	textarea.value = textarea.value.replace(textSelected,textSelected.bold());
-	
+		negrito.classList.toggle('toggleColor')
+	 textarea.innerHTML = textarea.innerText.toString().replace(textSelected,textSelected.toString().bold())
+
 })
+italico.addEventListener('click',()=>{
+		italico.classList.toggle('toggleColor')
+	 textarea.innerHTML = textarea.innerText.toString().replace(textSelected,textSelected.toString().italics())
+
+})
+sublinhado.addEventListener('click',()=>{
+		sublinhado.classList.toggle('toggleColor')
+	textarea.innerHTML = textarea.innerText.toString().replace(textSelected,"<u>"+textSelected.toString()+"</u>")
+
+})
+
